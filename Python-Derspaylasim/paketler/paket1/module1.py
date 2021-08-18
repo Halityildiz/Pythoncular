b9478e76614e0750c55376c8ff5611e6480d4103
halit <halatsar35@gmail.com>
2021-08-19 00:02:46 +0300

    moduller updated

---
+++ Python-Derspaylasim/paketler/paket1/module1.py
@@ -0,0 +1,7 @@
+'''
+This is module 1. Welcome!
+'''
+def addition(a,b):
+    return a + b
+def multi(x,y):
+    return x * y
---
+++ Python-Derspaylasim/paketler/paket2/module2.py
@@ -0,0 +1,7 @@
+'''
+This is module 2. Welcome!
+'''
+def divide(a,b):
+    return (a / b)
+def hello():
+    print("hello")
