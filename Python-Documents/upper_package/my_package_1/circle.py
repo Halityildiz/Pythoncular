4129758a34b74bc7247425174d049055084049a0
halit <halatsar35@gmail.com>
2021-08-23 15:14:15 +0300

    non repeat string cozulerek eklendi

---
+++ Python-Documents/upper_package/my_package_1/circle.py
@@ -0,0 +1,18 @@
+"""
+This modul calculate area of the circle and around of the circle.
+
+
+"""
+
+
+
+pi = 3.14
+
+
+def circlearea(r):
+    area = pi * r **2 #r = radius
+    return area
+
+def aroundcircle(r):
+    around = 2 * pi * r #r = radius
+    return around
